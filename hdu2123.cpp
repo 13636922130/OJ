fa855b46ad53593d2f5afcbb4bfcb3d6f3783af6
YiQun Chen <c872534679@qq.com>
2018-11-22 16:18:52 +0800

    hdu2123

---
+++ hdu2123.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int t;
+
+    scanf("%d", &t);
+    while(t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                printf("%d", i*j);
+                if(j != n) printf(" ");
+            }
+
+            printf("\n");
+        }
+    }
+
+}
